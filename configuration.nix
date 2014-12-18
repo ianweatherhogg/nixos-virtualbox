1e7f02137584f0722a2f7ed0fac3c8bd55ac7de5
Ian Weatherhogg <ian@ianweatherhogg.com>
2014-12-18 10:38:30 +0000

    confussion between desktop and display managers - corrected

--- configuration.nix
+++ configuration.nix
@@ -62,36 +62,38 @@
     # };
   };
 
-  services = {
-    xserver = {
-      autorun = true;
-      desktopManager = {
-        slim = {
-          defaultUser = "ian";
-          enable = true;
-          theme = pkgs.fetchurl {
-            url = https://github.com/jagajaga/nixos-slim-theme/archive/1.0.tar.gz;
-            sha256 = "08ygjn5vhn3iavh36pdcb15ij3z34qnxp20xh3s1hy2hrp63s6kn";
-          };
-        };#slim
-        xterm.enable = false;
-        default = "none";
-        sessionCommands = "
-           xmodmap -e 'pointer = 3 2 1 5 4 7 6 8 9 10 11 12'
-           /run/current-system/sw/bin/urxvtd -q -o -f
-        ";#TODO urxvt as systemd and no keychain
-      };#displayManager
+  services.xserver = {
+    autorun = true;
+    displayManager.slim = {
+      defaultUser = "ian";
       enable = true;
-      layout = "gb";
-      startGnuPGAgent = true;
-      windowManager = {
-       i3.enable = true;
-       default = "i3";
+      theme = pkgs.fetchurl {
+        url = https://github.com/jagajaga/nixos-slim-theme/archive/1.0.tar.gz;
+        sha256 = "08ygjn5vhn3iavh36pdcb15ij3z34qnxp20xh3s1hy2hrp63s6kn";
       };
-    };#xserver
+    };
+    displayManager.sessionCommands = ''
+       xmodmap -e 'pointer = 3 2 1 5 4 7 6 8 9 10 11 12'
+       /run/current-system/sw/bin/urxvtd -q -o -f
+    '';
+    desktopManager = {
+      xterm.enable = false;
+      default = "none";
+    };
+    enable = true;
+    layout = "gb";
+    startGnuPGAgent = true;
+    windowManager = {
+     i3.enable = true;
+     default = "i3";
+    };
+  };
+
+  services = {
     cron.enable = false;
     openssh.enable = true;
-  };#services
+  };
+
 
   environment.systemPackages = with pkgs; [
     git
